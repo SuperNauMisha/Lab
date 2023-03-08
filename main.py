3753198b50be295cbd7a18fb6a05586b2e59b18b
Михаил <naum0405072gmail.com>
2023-03-08 19:06:20 +0300

    Данные о пациенте, запись их в таблицу и сохрание с дефолтным названием

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
+import datetime
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDateTimeEdit, QPushButton
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt5.QtCore import QIODevice
 from pyqtgraph import PlotWidget
@@ -10,17 +11,19 @@ from openpyxl.chart import BarChart, Reference
 from PyQt5.QtWidgets import QFileDialog
 
 
-
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
+        self.dt_now = datetime.datetime.today()
+        self.dateTimeEdit.setDateTime(self.dt_now)
         self.interferences = 0
-        self.connectButton.clicked.connect(self.onConnect)
-        self.disconnectButton.clicked.connect(self.onDisconnect)
+        self.connectButton.clicked.connect(self.buttonConDis)
         self.saveButton.clicked.connect(self.save)
+        self.clearButton.clicked.connect(self.onClear)
         self.serial = QSerialPort()
         self.serial.setBaudRate(9600)
+        self.isConnected = False
 
         self.interferences = 0
         self.data_list = []
@@ -42,7 +45,13 @@ class MyWidget(QMainWindow):
         self.ports.addItems(self.ports_name_list)
         self.serial.readyRead.connect(self.onRead)
 
-
+    def buttonConDis(self):
+        if self.connectButton.text() == "Начать":
+            self.connectButton.setText("Остановить")
+            self.onConnect()
+        elif self.connectButton.text() == "Остановить":
+            self.connectButton.setText("Начать")
+            self.onDisconnect()
 
     def onConnect(self):
         print("connect")
@@ -54,8 +63,22 @@ class MyWidget(QMainWindow):
         self.serial.readyRead.connect(self.onRead)
 
     def onDisconnect(self):
+        print("disconnect")
         self.serial.close()
 
+    def onClear(self):
+        self.interferences = 0
+        self.oldstrok_data = ''
+        self.strok_data = ''
+        self.data_list = []
+        self.time = []
+        self.now_time = 0
+        self.nameEdit.setText('')
+        self.numEdit.setText('')
+        self.diagnosisEdit.clear()
+        self.conditionEdit.clear()
+        self.dt_now = datetime.datetime.today()
+        self.dateTimeEdit.setDateTime(self.dt_now)
 
     def onRead(self):
         try:
@@ -80,21 +103,33 @@ class MyWidget(QMainWindow):
             print("err", err)
 
     def save(self):
+        named_data_patient = ["Дата и время", "ФИО", "№ Истории болезни", "Диагноз", "Обстоятельства"]
+        data_patient = [self.dateTimeEdit.dateTime().toString('dd.MM.yyyy hh:mm'), self.nameEdit.text(),\
+                        self.numEdit.text(), self.diagnosisEdit.toPlainText(), self.conditionEdit.toPlainText()]
         wb = Workbook()
         wb.create_sheet(title='Первый лист', index=0)
         sheet = wb['Первый лист']
-        for col in range(len(self.data_list)):
-            cell = sheet.cell(row=col + 1, column=2)
-            cell.value = self.data_list[col]
-
+        print(data_patient[0])
         for col in range(len(self.time)):
             cell = sheet.cell(row=col + 1, column=1)
             cell.value = self.time[col]
 
-        filename = QFileDialog.getSaveFileName(self, "Сохранить в таблицу", "", "*.xlsx")
+        for col in range(len(self.data_list)):
+            cell = sheet.cell(row=col + 1, column=2)
+            cell.value = self.data_list[col]
+
+        for col in range(len(named_data_patient)):
+            cell = sheet.cell(row=col + 1, column=3)
+            cell.value = named_data_patient[col]
+
+        for col in range(len(data_patient)):
+            cell = sheet.cell(row=col + 1, column=4)
+            cell.value = data_patient[col]
+
+        filename = QFileDialog.getSaveFileName(self, "Сохранить в таблицу", str(data_patient[1].split()[0]) + '_' +\
+                                               self.dateTimeEdit.dateTime().toString('dd-MM-yyyy_hh-mm'), "*.xlsx")
         print(filename)
         wb.save(filename[0])
-
 
 
 if __name__ == '__main__':
