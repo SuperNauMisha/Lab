2bdbd9a5b3670375982dc8ebf859316870a58689
Михаил <naum0405072gmail.com>
2023-03-19 01:21:52 +0300

    Доп данные + сохр в Exel

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDateTimeEdit, QPushButton
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt5.QtCore import QIODevice
+from PyQt5.QtCore import Qt
 from pyqtgraph import PlotWidget
 import pyqtgraph as pg
 import openpyxl
@@ -19,7 +20,7 @@ class MyWidget(QMainWindow):
 
 
         self.maxTopValue = 250
-        self.maxRightValue = 1600
+        self.maxRightValue = 2000
 
 
         self.setWindowTitle("Коагулограф")
@@ -100,6 +101,7 @@ class MyWidget(QMainWindow):
         self.numEdit.setText('')
         self.diagnosisEdit.clear()
         self.conditionEdit.clear()
+        self.output.clear()
         self.dt_now = datetime.datetime.today()
         self.dateTimeEdit.setDateTime(self.dt_now)
         self.dateTimeEdit_2.setDateTime(self.dt_now)
@@ -118,7 +120,6 @@ class MyWidget(QMainWindow):
             if r"\n" not in self.strok_data:
                 self.oldstrok_data += self.strok_data
             else:
-                print(str(self.oldstrok_data + self.strok_data)[0:-4])
                 if self.interferences < 10:
                     self.interferences += 1
                     self.oldstrok_data = ''
@@ -142,7 +143,6 @@ class MyWidget(QMainWindow):
         wb = openpyxl.Workbook()
         wb.create_sheet(title='Первый лист', index=0)
         sheet = wb['Первый лист']
-        print(data_patient[0])
         for row in range(len(self.time)):
             cell = sheet.cell(row=row + 1, column=1)
             cell.value = self.time[row]
@@ -160,7 +160,6 @@ class MyWidget(QMainWindow):
         for row in range(len(all_data)):
             cell = sheet.cell(row=row + 1, column=4)
             cell.value = all_data[row]
-        print(data_patient)
         try:
             filename = QFileDialog.getSaveFileName(self, "Сохранить в таблицу",
                                                    str(self.nameEdit.text().split()[0]) + '_' +
@@ -179,7 +178,6 @@ class MyWidget(QMainWindow):
         wb = openpyxl.load_workbook(filename[0])
         sh_names = wb.sheetnames
         ind = self.numListEdit.value()
-        print(ind)
         sheet = wb[sh_names[ind - 1]]
         row = 1
         while True:
@@ -236,7 +234,7 @@ class MyWidget(QMainWindow):
             datanorm = self.contour(data, period) #перестраиваем на верхние и нижние пики. datanorm - четырёхмерный массив (NumPy):
             #(datanorm[0, :] - время верхних пиков, сек; datanorm[1, :] - значение верхних пиков; datanorm[2, :] - время нижних пиков, сек; datanorm[3, :] - значение нижних пиков).
             zeroboard = self.zeropoint(datanorm, np.min(data[1, :])) #граница ухода с начального плато (плато нулей) (одномерный массив (NumPy): [0] - координата границы, сек; [1] - индекс этой точки в datanorm).
-            deltamin = self.mindeltapoint(datanorm, zeroboard[1]) # точка с минимальной шириной графика (одномерный массив (NumPy): [0] - ширина; [1] - координата, сек).
+            deltamin = self.mindeltapoint(datanorm, zeroboard[1]) # точка с минимальной шириной графика (одномерный массив (NumPy): [0] - ширина; [1] - координата, сек; [2] - индекс точки в datanorm).
             plato = self.platopoint(datanorm, zeroboard[1], deltamin, sigma) # границы центрального плато (возле deltamin) plato - двухмерный массив (NumPy):
             #([0/1/2, 0]- координата левой/правой/трёхминутной границы, сек; [0/1/2, 1] - ширина графика в точке левой/правой/трёхминутной границы).
             #трёхминутная граница - точка, отстоящая от правой границы плато на 3 минуты.
@@ -245,26 +243,69 @@ class MyWidget(QMainWindow):
             # любой из элементоф графика можно отключить, закомментировав его
             # self.graph.plot(data[0,:],data[1,:])
 
-            self.graph_data = [zeroboard[0], deltamin[0], deltamin[1], plato[0, 1], plato[0, 0], plato[1, 1], plato[1, 0],
-                               plato[2, 1], plato[2, 0]]
 
-            self.named_graph_data = ["Граница нулевого плато, сек",
-                                     "Минимальная ширина графика", "Время минимальная ширина графика, сек",
-                                     "Ширина левой границы плато", "Время левая границы плато, сек",
-                                     "Ширина правой границы плато", "Время правой границы плато, сек",
-                                     "Ширина через 3 минуты после правой границы плато", "Время через 3 минуты после правой границы плато"]
 
             self.graph.plot(self.time, norm_data_list, pen=self.pen)
             self.graph.plot(datanorm[0,:],datanorm[1,:], pen=pg.mkPen(color=(0, 0, 255)))
             self.graph.plot(datanorm[2, :], datanorm[3, :], name="Границы", pen=pg.mkPen(color=(0, 0, 255)))
-            self.graph.plot([zeroboard[0], zeroboard[0]], [np.min(data[1, :])-10, np.max(data[1, :])+10], name=f'Граница нулевого плато, t = {zeroboard[0]} сек', pen=pg.mkPen(color=(142, 61, 0), width=2))
-            self.graph.plot([deltamin[1], deltamin[1]], [np.min(data[1, :])-10, np.max(data[1, :])+10], name=f'Минимальная ширина графика, ширина = {deltamin[0]}, t = {deltamin[1]} сек', pen=pg.mkPen(color=(255, 0, 255), width=2))
-            self.graph.plot([plato[0, 0], plato[0, 0]], [np.min(data[1, :])-10, np.max(data[1, :])+10], name=f'Левая граница плато, ширина = {plato[0, 1]}, t = {plato[0, 0]} сек', pen=pg.mkPen(color=(255, 100, 166), width=2))
-            self.graph.plot([plato[1, 0], plato[1, 0]], [np.min(data[1, :])-10, np.max(data[1, :])+10], name=f'Правая граница плато, ширина = {plato[1, 1]}, t = {plato[1, 0]} сек', pen=pg.mkPen(color=(255, 165, 0), width=2))
-            self.graph.plot([plato[2, 0], plato[2, 0]], [np.min(data[1, :])-10, np.max(data[1, :])+10], name=f'3 минуты после правой границы плато, ширина = {plato[2, 1]}, t = {plato[2,0]} сек', pen=pg.mkPen(color=(0, 100, 100), width=2))
+            self.graph.plot([zeroboard[0], zeroboard[0]], [np.min(data[1, :])-10, np.max(data[1, :])+10], name=f'Время до начала свёртывания, t = {zeroboard[0]} сек', pen=pg.mkPen(color=(0, 0, 0), width=2))
+            self.graph.plot([deltamin[1], deltamin[1]], [np.min(data[1, :])-10, np.max(data[1, :])+10], name=f'Время до окончания свёртывания, t = {deltamin[1]} сек', pen=pg.mkPen(color=(255, 0, 255), width=2))
+            self.graph.plot([plato[0, 0], plato[0, 0]], [np.min(data[1, :])-10, np.max(data[1, :])+10], name=f'Время до начала ретракции, t = {plato[0, 0]} сек', pen=pg.mkPen(color=(255, 100, 166), width=2, style=Qt.DashLine))
+            self.graph.plot([plato[1, 0], plato[1, 0]], [np.min(data[1, :])-10, np.max(data[1, :])+10], name=f'Время окончания ретракции, t = {plato[1, 0]} сек', pen=pg.mkPen(color=(255, 165, 0), width=2, style=Qt.DashLine))
+            self.graph.plot([plato[2, 0], plato[2, 0]], [np.min(data[1, :])-10, np.max(data[1, :])+10], name=f'Амплитуда на 3 минуте фибринолиза = {plato[2, 1]}', pen=pg.mkPen(color=(0, 100, 100), width=2, style=Qt.DashDotDotLine))
             self.graph.showGrid(x=True, y=True)
-        except:
-            print("err")
+
+
+
+
+            self.named_graph_data = ["Время свёртывания, сек",
+                                     "Время до начала свёртывания, сек",
+                                     "Время до окончания свёртывания, сек",
+                                     "Время до начала ретракции, сек",
+                                     "Время окончания ретракции, сек",
+                                     "Максимальная амплитуда, ед",
+                                     "Минимальная амплитуда, ед",
+                                     "Амплитуда на 3 минуте фибринолиза, ед",
+                                     "Скорость свёртывания, ед/мин",
+                                     "Скорость нарастания фибринолиза, ед/мин",
+                                     "Коэффицент ректракции, %",
+                                     "Коэффицент фибринолиза, %"]
+
+
+            t_clotting = round(plato[0, 0] - zeroboard[0], 2) #Время свёртывания, сек
+            v_clotting = round((datanorm[1, zeroboard[1]] - plato[0, 1]) / t_clotting * 60, 2) #Скорость свёртывания, ед/сек
+            cof_retr = round((datanorm[1, zeroboard[1]] - deltamin[0]) / datanorm[1, zeroboard[1]] * 100, 2) #Коэффицент ректракции, %
+            v_fibrin = round((plato[2, 1] - plato[1, 1]) / 180 * 60, 2) #Скорость нарастания фибринолиза, ед/сек
+            cof_fibrin = round((plato[2, 1] - deltamin[0]) / plato[2, 1] * 100, 2) #Коэффицент фибринолиза, %
+
+
+            self.graph_data = [t_clotting, zeroboard[0], deltamin[1], plato[0, 0], plato[1, 0],
+                               datanorm[1, zeroboard[1]], deltamin[0], plato[2, 1],
+                               v_clotting, v_fibrin,
+                               cof_retr, cof_fibrin]
+
+            strok_output = "Время свёртывания, сек" + " " * (40 - len("Время свёртывания, сек")) + str(t_clotting) + "\n"
+            strok_output += "Время до начала свёртывания, сек" + " " * (40 - len("Время до начала свёртывания, сек")) + str(zeroboard[0]) + "\n"
+            strok_output += "Время до окончания свёртывания, сек" + " " * (40 - len("Время до окончания свёртывания, сек")) + str(deltamin[1]) + "\n"
+            strok_output += "Время до начала ретракции, сек" + " " * (40 - len("Время до начала ретракции, сек")) + str(plato[0, 0]) + "\n"
+            strok_output += "Время окончания ретракции, сек" + " " * (40 - len("Время окончания ретракции, сек")) + str(plato[1, 0]) + "\n"
+
+            strok_output += "Максимальная амплитуда, ед" + " " * (40 - len("Максимальная амплитуда, ед")) + str(datanorm[1, zeroboard[1]]) + "\n"
+            strok_output += "Минимальная амплитуда, ед" + " " * (40 - len("Минимальная амплитуда, ед")) + str(deltamin[0]) + "\n"
+            strok_output += "Амплитуда на 3 минуте фибринолиза, ед" + " " * (40 - len("Амплитуда на 3 минуте фибринолиза, ед")) + str(plato[2, 1]) + "\n"
+
+            strok_output += "Скорость свёртывания, ед/мин" + " " * (40 - len("Скорость свёртывания, ед/мин")) + str(v_clotting) + "\n"
+            strok_output += "Скорость нарастания фибринолиза, ед/мин" + " " * (40 - len("Скорость нарастания фибринолиза, ед/мин")) + str(v_fibrin) + "\n"
+
+            strok_output += "Коэффицент ректракции, %" + " " * (40 - len("Коэффицент ректракции, %")) + str(cof_retr) + "\n"
+            strok_output += "Коэффицент фибринолиза, %" + " " * (40 - len("Коэффицент фибринолиза, %")) + str(cof_fibrin) + "\n"
+
+
+            self.output.setPlainText(strok_output)
+        except Exception as err:
+            print(err)
+
+
 
 
 
@@ -300,38 +341,41 @@ class MyWidget(QMainWindow):
 
     def mindeltapoint(self, datanorm, zeroindex): #zeroindex = zeroboard[1]
         height = np.max(datanorm[1,:])-np.min(datanorm[1, :])
-        deltamin = np.array([height, 0])
+        deltamin = np.array([height, 0, 0])
         for countnorm in range(zeroindex,len(datanorm[1, :])):
             delta = datanorm[1, countnorm] - datanorm[3, countnorm]
             if (delta < deltamin[0]):
                 deltamin[0] = delta
                 deltamin[1] = (datanorm[0, countnorm]+datanorm[2, countnorm])/2
+                deltamin[2] = countnorm
         return deltamin
 
 
     def platopoint(self, datanorm, zeroindex, deltamin, sigma):
-        plato = np.array([[0,0], [0, 0], [0,0]])
+        plato = np.array([[deltamin[1],deltamin[0]], [deltamin[1], deltamin[0]], [0,0]])
         stopper = 0
-        rightindex = 0
-        for i in range(zeroindex, len(datanorm[1, :])):
+        rightindex = int(deltamin[2])
+        for i in range (int(deltamin[2]), int(zeroindex), -1):
             deltanext = datanorm[1, i] - datanorm[3, i]
             delta = datanorm[1, i-1] - datanorm[3, i-1]
             deltalast = datanorm[1, i-2] - datanorm[3, i-2]
             if (delta <= deltamin[0]*(1+sigma)) and (deltanext <= deltamin[0]*(1+sigma)) and (deltalast <= deltamin[0]*(1+sigma)):
-                if stopper == 0 :
-                    stopper = 1
-                    plato[0,0] = (datanorm[0,i-2]+datanorm[2, i-2])/2
-                    plato[0,1] = deltalast
+                plato[0,0] = (datanorm[0,i-2]+datanorm[2, i-2])/2
+                plato[0,1] = deltalast
+
+        for i in range (int(deltamin[2]), len(datanorm[1, :])):
+            deltanext = datanorm[1, i] - datanorm[3, i]
+            delta = datanorm[1, i-1] - datanorm[3, i-1]
+            deltalast = datanorm[1, i-2] - datanorm[3, i-2]
+            if (delta <= deltamin[0]*(1+sigma)) and (deltanext <= deltamin[0]*(1+sigma)) and (deltalast <= deltamin[0]*(1+sigma)):
                 plato[1,0] = (datanorm[0,i]+datanorm[2, i])/2
                 plato[1,1] = deltanext
                 rightindex = i
+
         rightindex = np.min([rightindex+18, len(datanorm[1, :])-1])
         plato[2,0] = (datanorm[0,rightindex]+datanorm[2, rightindex])/2 #+3минуты
         plato[2,1] = datanorm[1, rightindex] - datanorm[3, rightindex]
         return plato
-
-
-
 
 
 if __name__ == '__main__':
